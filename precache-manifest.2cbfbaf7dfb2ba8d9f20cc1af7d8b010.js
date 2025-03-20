c08aaa7c17b6f5cec8768d6dbaa2c9de1fd88fa9
joebags99 <jckonkle99@gmail.com>
2025-03-19 20:03:34 -0500

    Updates

--- precache-manifest.2cbfbaf7dfb2ba8d9f20cc1af7d8b010.js
+++ precache-manifest.2cbfbaf7dfb2ba8d9f20cc1af7d8b010.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a417c38c48ccdcde892ace1083e9310f",
+    "revision": "0f4a90eb22c2212c1798165aa00f1dde",
     "url": "/ederian-travel-tracker/index.html"
   },
   {
-    "revision": "866ba2e5c535f93a3e90",
+    "revision": "0ecc38550bfeae162532",
     "url": "/ederian-travel-tracker/static/css/main.4a1d8ed7.chunk.css"
   },
   {
-    "revision": "c59342286ad5f5fd2dd9",
-    "url": "/ederian-travel-tracker/static/js/2.2c6d8870.chunk.js"
+    "revision": "b8dc2c69d3d21ca650f0",
+    "url": "/ederian-travel-tracker/static/js/2.18d84714.chunk.js"
   },
   {
     "revision": "487760c8630fcdafe7a50fa9270739a3",
-    "url": "/ederian-travel-tracker/static/js/2.2c6d8870.chunk.js.LICENSE.txt"
+    "url": "/ederian-travel-tracker/static/js/2.18d84714.chunk.js.LICENSE.txt"
   },
   {
     "revision": "b31c97af6a3ccfe5746b",
     "url": "/ederian-travel-tracker/static/js/3.691db69f.chunk.js"
   },
   {
-    "revision": "866ba2e5c535f93a3e90",
-    "url": "/ederian-travel-tracker/static/js/main.aa00e165.chunk.js"
+    "revision": "0ecc38550bfeae162532",
+    "url": "/ederian-travel-tracker/static/js/main.cbb2b599.chunk.js"
   },
   {
     "revision": "fdfabd3cc4ac7d4807c4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ederian-travel-tracker/precache-manifest.0743e0c3d6f124e0521d57a78edf344c.js"
+  "/ederian-travel-tracker/precache-manifest.2cbfbaf7dfb2ba8d9f20cc1af7d8b010.js"
 );
 
 self.addEventListener('message', (event) => {
